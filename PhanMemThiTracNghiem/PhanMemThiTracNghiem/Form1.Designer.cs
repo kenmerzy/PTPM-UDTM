93ec50769d186155a51f76c88c0e782255e634e4
Ngọc Long <65071189+kenmerzy@users.noreply.github.com>
2020-06-16 19:16:14 +0700

    ThemLabel

--- PhanMemThiTracNghiem/PhanMemThiTracNghiem/Form1.Designer.cs
+++ PhanMemThiTracNghiem/PhanMemThiTracNghiem/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Location = new System.Drawing.Point(332, 63);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(35, 17);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(951, 493);
+            this.Controls.Add(this.lbl_Title);
+            this.Name = "Form1";
+            this.Text = "Phần mềm thi trắc nghiệm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Title;
     }
 }
 
